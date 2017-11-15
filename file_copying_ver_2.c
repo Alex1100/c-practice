088b5a88ac71bdbdcadf058dad09a7477307ec7a
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-11-14 22:10:46 -0800

    add file copying version 2

--- file_copying_ver_2.c
+++ file_copying_ver_2.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 main(){
   int c;
+  char word[255];
+  int i = 0;
 
   while((c = getchar()) != EOF){
-    putchar(c);
+    word[i] = putchar(c);
+    i = i + 1;
+    printf("\nDid you say? %s\n", word);
   }
+
+  i = 0;
+  memset(word, 0, sizeof word);
 }
